2b4f61cdc12a2b308bcf0a5d0a354d489cb68ef7
Todd Demone <tdemone1@gmail.com>
2024-10-03 12:06:33 -0400

    rename TodoForm.jsx to AddForm.jsx

--- src/AddForm.jsx
+++ src/AddForm.jsx
@@ -1,28 +1,24 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-function TodoForm({ onAddSubmit }) {
+export default function AddForm({ onAddSubmit }) {
   const [addInput, setAddInput] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddSubmit(addInput);
+    setAddInput("");
+  }
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onAddSubmit(addInput);
-        setAddInput("");
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         value={addInput}
-        onChange={(e) => {
-          setAddInput(e.target.value);
-        }}
+        onChange={(e) => setAddInput(e.target.value)}
         placeholder="Add new todo"
       />
       <button type="submit">Add New Todo</button>
     </form>
   );
 }
-
-export default TodoForm;
